e0344b1bf020d0bfd975976a1b93b67b32e05da5
huanxinding <278858563@qq.com>
2015-03-27 17:34:37 +0800

    2015-03-27

---
+++ src/Nd.Framework/Application/AppRuntime.cs
@@ -0,0 +1,74 @@
+﻿using Nd.Framework.Configuration;
+using Nd.Framework.Core;
+using Nd.Framework.Logging;
+
+namespace Nd.Framework.Application
+{
+    /// <summary>
+    /// 应用运行时
+    /// </summary>
+    public sealed class AppRuntime
+    {
+        #region 私有字段
+        private static readonly AppRuntime instance = new AppRuntime();
+        private static readonly object lockObj = new object();
+
+        private IApp currentApplication = null;
+        #endregion
+
+        #region 公共属性
+        public static AppRuntime Instance
+        {
+            get { return instance; }
+        }
+
+        /// <summary>
+        /// 当前应用
+        /// </summary>
+        public IApp CurrentApplication
+        {
+            get { return currentApplication; }
+        }
+
+        /// <summary>
+        /// IOC容器
+        /// </summary>
+        public INdContainer Container
+        {
+            get { return this.currentApplication.ObjectContainer; }
+        }
+
+        /// <summary>
+        /// 日志记录器
+        /// </summary>
+        public ILogger Logger
+        {
+            get { return this.currentApplication.Logger; }
+        }
+        #endregion
+
+        #region 公共方法
+        /// <summary>
+        /// 创建一个新的应用
+        /// </summary>
+        /// <param name="configSource">应用配置</param>
+        /// <returns></returns>
+        public static IApp Create(IConfigSource configSource)
+        {
+            lock (lockObj)
+            {
+                if (instance.currentApplication == null)
+                {
+                    instance.currentApplication = new App(configSource);
+                }
+            }
+            return instance.currentApplication;
+        }
+        #endregion
+
+        #region 构造函数
+        static AppRuntime() { }
+        private AppRuntime() { }
+        #endregion
+    }
+}
