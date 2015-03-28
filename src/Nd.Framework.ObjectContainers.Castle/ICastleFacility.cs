8fad43d4a24e62b6575efd9b96dbc86c14bd4e92
huanxinding <278858563@qq.com>
2015-03-28 15:46:48 +0800

    2015-03-28

---
+++ src/Nd.Framework.ObjectContainers.Castle/ICastleFacility.cs
@@ -0,0 +1,10 @@
+﻿using Castle.MicroKernel;
+using Nd.Framework.Core;
+
+namespace Nd.Framework.ObjectContainers.Castle
+{
+    public interface ICastleFacility : INdFacility, IFacility
+    {
+
+    }
+}
---
+++ src/Nd.Framework.ObjectContainers.Castle/ICastleInterceptor.cs
@@ -0,0 +1,9 @@
+﻿using Castle.DynamicProxy;
+using Nd.Framework.Core;
+
+namespace Nd.Framework.ObjectContainers.Castle
+{
+    public interface ICastleInterceptor : INdInterceptor, IInterceptor
+    {
+    }
+}
