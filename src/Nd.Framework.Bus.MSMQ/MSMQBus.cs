43e30f2d50e21739aa8b0034005e46e94913f30c
huanxinding <278858563@qq.com>
2015-04-27 14:59:00 +0800

    2015-04-27

---
+++ src/Nd.Framework.Bus.MSMQ/MSMQBus.cs
@@ -0,0 +1,61 @@
+﻿using Nd.Framework.Core;
+
+namespace Nd.Framework.Bus.MSMQ
+{
+    /// <summary>
+    /// 基于消息队列的消息总线基类
+    /// </summary>
+    public abstract class MSMQBus : DisposableObject, IBus
+    {
+        #region 私有字段
+
+        #endregion
+
+        #region 构造函数
+
+        #endregion
+
+        #region 受保护的方法
+
+        #endregion
+
+        #region IBus 成员
+        public void Publish<TMessage>(TMessage message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Publish<TMessage>(System.Collections.Generic.IEnumerable<TMessage> message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+
+        #region IUnitOfWork 成员
+        public bool DistributedTransactionSupported
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public bool Committed
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public void Commit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Rollback()
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+    }
+}
