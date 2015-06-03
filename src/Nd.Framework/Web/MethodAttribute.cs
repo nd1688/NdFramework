3d089e9d8736f7f3ef14809c715bed07ca2193c2
huanxinding <278858563@qq.com>
2015-06-03 09:20:25 +0800

    20150603

---
+++ src/Nd.Framework/Web/MethodAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Nd.Framework.Web
+{
+    /// <summary>
+    /// 接口方法特性
+    /// </summary>
+    public class MethodAttribute : Attribute
+    {
+        #region 私有字段
+        private string methodName;
+        #endregion
+
+        #region 构造函数
+        public MethodAttribute(string methodName)
+        {
+            this.methodName = methodName;
+        }
+        #endregion
+
+        #region 公共属性
+        public string MethodName
+        {
+            get { return this.methodName; }
+            set { this.methodName = value; }
+        }
+        #endregion
+    }
+}
